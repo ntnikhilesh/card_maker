3dbfc11d5cea1099fd1206c309a91ec34929282f
Nikhi <ntnikhilesh@gmail.com>
2016-11-03 20:10:31 +0530

    fetch json and parse using pojo

---
+++ app/src/main/java/com/example/dell/mdemo/interfaces/MaptagServiceInterface.java
@@ -0,0 +1,21 @@
+package com.example.dell.mdemo.interfaces;
+
+//import com.example.hp.movies.apimodel.MovieDb;
+
+import com.example.dell.mdemo.LoginModel;
+
+import retrofit.Callback;
+import retrofit.client.Response;
+import retrofit.http.Field;
+import retrofit.http.FormUrlEncoded;
+import retrofit.http.POST;
+
+/**
+ * Created by hp on 20-02-2016.
+ */
+public interface MaptagServiceInterface {
+    @FormUrlEncoded
+    @POST("/login")
+    void confirmSlot(@Field("email")String  uname,@Field("password")String  password, Callback<LoginModel> responseCallback);
+
+}
