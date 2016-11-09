75a4ca3dfd48ec2056b16915114fdfe83bb70fb3
nikhil <ntnikhilesh@gmail.com>
2016-11-09 23:16:45 +0530

    start card making app now

--- app/src/main/java/com/example/dell/mdemo/MainActivity.java
+++ app/src/main/java/com/example/dell/mdemo/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity
 
     private MaptagServiceInterface mv;
 
+//Login page
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
