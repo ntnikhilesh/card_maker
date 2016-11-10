3b61a27ad15148ab198d2e5461d566472863bd8a
nikhil <ntnikhilesh@gmail.com>
2016-11-11 00:39:00 +0530

    tv added in design page

--- app/src/main/java/com/example/dell/mdemo/SelectDesignActivity.java
+++ app/src/main/java/com/example/dell/mdemo/SelectDesignActivity.java
@@ -6,7 +6,8 @@ import android.widget.TextView;
 
 public class SelectDesignActivity extends AppCompatActivity {
 
-    TextView wc_detail;
+    TextView wc_detail,pyour_name,ppartner_name,pwedding_message,pdate_time,plocation;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +15,28 @@ public class SelectDesignActivity extends AppCompatActivity {
         setContentView(R.layout.activity_select_design);
 
 
-    wc_detail=(TextView)findViewById(R.id.tv_wedding_deteal);
+    //wc_detail=(TextView)findViewById(R.id.tv_wedding_deteal);
+
+        pyour_name=(TextView)findViewById(R.id.tv_w3_your_name);
+        ppartner_name=(TextView)findViewById(R.id.tv_w5_partner_name);
+        pwedding_message=(TextView)findViewById(R.id.tv_w6_wedding_message);
+        pdate_time=(TextView)findViewById(R.id.tv_w7_time);
+        plocation=(TextView)findViewById(R.id.tv_w8_location);
+
+
         String nyour_name= getIntent().getStringExtra("your name");
         String nparter_name= getIntent().getStringExtra("partner name");
         String ndate_time= getIntent().getStringExtra("dt");
         String nwedding_msg= getIntent().getStringExtra("msg");
         String nlocation= getIntent().getStringExtra("location");
 
-        wc_detail.setText("Wedding Invitation \n Together with their families"+nyour_name+"\n weds \n"+nparter_name+"\n"+nwedding_msg+"\n"+ndate_time+"\nLocation \n"+nlocation);
+        pyour_name.setText(nyour_name);
+        ppartner_name.setText(nparter_name);
+        pwedding_message.setText(nwedding_msg);
+        pdate_time.setText(ndate_time);
+        plocation.setText(nlocation);
+
+       // wc_detail.setText("Wedding Invitation \n Together with their families"+nyour_name+"\n weds \n"+nparter_name+"\n"+nwedding_msg+"\n"+ndate_time+"\nLocation \n"+nlocation);
 
     }
 }
